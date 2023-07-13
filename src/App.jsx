a5fa78364d3d87336a9785b92a80536c53020665
Iamtheusername112 <apkfem@gmail.com>
2023-07-13 09:41:26 +0200

    solved lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
 import "./App.css";
+import foodsJson from "./foods.json";
+import FoodBox from "./components/FoodBox";
+import AddFoodForm from "./components/AddFoodForm";
 
 function App() {
+  const [foods, setFoods] = useState(foodsJson);
+
+  const deleteFoods = (currentFoodId) => {
+    setFoods((prevFoods) =>
+      prevFoods.filter((food) => food.id !== currentFoodId)
+    );
+  };
+
+  const addFoods = (newFood) => {
+    setFoods((prevFoods) => [...prevFoods, newFood]);
+  };
+
   return (
-    <div className="App">
-      <h1>LAB | React IronNutrition</h1>
+    <div className="container">
+      <h1>Food Selection</h1>
+      <AddFoodForm addFoods={addFoods} />
+      {foods.map((food) => (
+        <div className="food-box" key={food.id}>
+          <FoodBox foods={food} deleteFoods={deleteFoods} />
+        </div>
+      ))}
     </div>
   );
 }
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,20 @@
-// Your code here
+import React from "react";
+
+function FoodBox(props) {
+  const totalCal = props.foods.servings * props.foods.calories;
+
+  return (
+    <div className="food-info">
+      <p>{props.foods.name}</p>
+      <img src={props.foods.image} alt={props.foods.name} />
+      <p>Calories: {props.foods.calories}</p>
+      <p>Servings: {props.foods.servings}</p>
+      <p>
+        <b>Total Calories: {totalCal} </b> kcal
+      </p>
+      <button onClick={() => props.deleteFoods(props.foods.id)}>Delete</button>
+    </div>
+  );
+}
+
+export default FoodBox;
